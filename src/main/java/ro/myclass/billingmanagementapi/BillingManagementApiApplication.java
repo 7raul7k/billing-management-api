20f8054af3aa6c8ff4a3e4bc454b7149d8eb5086
7raul7k <balanescuraul@gmail.com>
2023-08-16 00:50:11 +0300

    add main

---
+++ src/main/java/ro/myclass/billingmanagementapi/BillingManagementApiApplication.java
@@ -0,0 +1,13 @@
+package ro.myclass.billingmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingManagementApiApplication.class, args);
+	}
+
+}
