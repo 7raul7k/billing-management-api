8b8972bf752f32ea45c701c432eaae97f92cee4c
7raul7k <balanescuraul@gmail.com>
2023-08-18 14:32:41 +0300

    add PermissionRepo

--- src/main/java/ro/myclass/billingmanagementapi/repo/UsernameRepo.java
+++ src/main/java/ro/myclass/billingmanagementapi/repo/UsernameRepo.java
@@ -2,12 +2,14 @@ package ro.myclass.billingmanagementapi.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ro.myclass.billingmanagementapi.models.Role;
 import ro.myclass.billingmanagementapi.models.Username;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UsernameRepo extends JpaRepository<Username,Long> {
 
 
@@ -25,4 +27,7 @@ public interface UsernameRepo extends JpaRepository<Username,Long> {
 
     @Query("select u from Username u where u.role = ?1")
     List<Username> getUsernameByRole(Role role);
+
+    @Query("Select u from Username u")
+    List<Username> getAllUsername();
 }
