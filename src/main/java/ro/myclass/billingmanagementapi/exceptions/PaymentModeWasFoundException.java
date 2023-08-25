e32e96227d2918e4d327c9deb408da8f6db05cd5
7raul7k <balanescuraul@gmail.com>
2023-08-25 22:53:03 +0300

    add role-resource-test

--- src/main/java/ro/myclass/billingmanagementapi/exceptions/PaymentModeWasFoundException.java
+++ src/main/java/ro/myclass/billingmanagementapi/exceptions/PaymentModeWasFoundException.java
@@ -1,5 +1,9 @@
 package ro.myclass.billingmanagementapi.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class PaymentModeWasFoundException extends RuntimeException{
     public PaymentModeWasFoundException() {
         super("Payment mode was found");
